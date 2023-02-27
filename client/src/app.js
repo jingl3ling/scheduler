7a918ca9d2debc75569af82d073b8f934f96b796
Jing Huang <86860939+jingl3ling@users.noreply.github.com>
2023-02-27 15:31:32 -0500

    initial setup

---
+++ client/src/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Date from './date';
+
+export default function App(){
+    return(
+        <div>
+            Scheduler
+            <Date/>
+        </div>
+    )
+}
---
+++ client/src/date.js
@@ -0,0 +1,33 @@
+import React, {useState} from 'react';
+import dayjs from 'dayjs';
+import TextField from '@mui/material/TextField';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
+
+export default function App(){
+    const [value, setValue] = React.useState(dayjs(new Date().toLocaleString()));
+
+    const handleChange = (newValue) => {
+      setValue(newValue);
+    };
+
+    const disableDates = (date) => {
+        return date.getDay()===2 
+    }
+
+    return(
+        <div>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <DesktopDatePicker
+          label="Date desktop"
+          inputFormat="MM/DD/YYYY"
+          value={value}
+          onChange={handleChange}
+          renderInput={(params) => <TextField {...params} />}
+        //   shouldDisableDate={disableDates}
+          disablePast={true}
+        /></LocalizationProvider>
+        </div>
+    )
+}
